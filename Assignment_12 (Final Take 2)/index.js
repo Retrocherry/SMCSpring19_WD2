57b0460d25ed249108a980f28405c8a06c7b1afc
Gabe Silverstein <gabesilverstein@gmail.com>
2019-05-30 19:51:24 -0700

    Separated Final take 1 and 2
    Pulled take 1 commit, copied to create a file for take 1 of my final.

---
+++ Assignment_12 (Final Take 2)/index.js
@@ -0,0 +1,18 @@
+var hamburgerIcon = $('#hamburger-icon');
+var mobileMenuContainer = $('#mobile-menu');
+
+console.log(hamburgerIcon);
+
+// Mobile Menu Handler
+hamburgerIcon.click(toggleMobileMenu);
+
+
+function toggleMobileMenu() {
+    mobileMenuContainer.toggleClass('open');
+
+    // if has class name of 'open' {
+    //     remove open from selected element
+    // } else if it doesn't have class name of open {
+    //     add 'open' to select element
+    // }
+}
